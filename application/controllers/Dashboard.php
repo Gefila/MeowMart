e8138376371b2fb5f57ef7914010a747d6bb69a2
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2025-03-11 23:02:15 +0700

    pertemuan1

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller{
+    public function index()
+    {
+        echo "<h1>Selamat Datang Di Website Kami</h1>";
+    }
+}
+?>
