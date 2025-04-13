86c7148ec7e4243233bee312c4d108b9e50a5b05
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2025-04-13 18:26:55 +0700

    menambahkan dark mode

--- application/views/administrator/templates/sidebar.php
+++ application/views/administrator/templates/sidebar.php
@@ -2,7 +2,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
       <a href="<?= base_url('admin') ?>" class="brand-link">
-          <img src="<?= base_url('assets/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+          <img src="<?= base_url('assets/') ?>dist/img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
           <span class="brand-text font-weight-light">Gefila Store</span>
       </a>
 
@@ -14,7 +14,7 @@
                   <img src="<?= base_url('assets/') ?>dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
               </div>
               <div class="info">
-                  <a href="" class="d-block"><?= $admin['full_name'];?></a>
+                  <a href="" class="d-block"><?= $admin['full_name']; ?></a>
               </div>
           </div>
 
@@ -45,13 +45,13 @@
                       </a>
                       <ul class="nav nav-treeview">
                           <li class="nav-item">
-                              <a href="<?=base_url()?>admin/kategori" class="nav-link <?= ($this->uri->segment(2) == 'kategori') ? 'active' : '' ?>">
+                              <a href="<?= base_url() ?>admin/kategori" class="nav-link <?= ($this->uri->segment(2) == 'kategori') ? 'active' : '' ?>">
                                   <i class="far fa-circle nav-icon"></i>
                                   <p>Kategori</p>
                               </a>
                           </li>
                           <li class="nav-item">
-                              <a href="<?=base_url()?>admin/produk" class="nav-link">
+                              <a href="<?= base_url() ?>admin/produk" class="nav-link">
                                   <i class="far fa-circle nav-icon"></i>
                                   <p>Produk</p>
                               </a>
@@ -65,7 +65,7 @@
                       </ul>
                   </li>
                   <li class="nav-item">
-                      <a href="<?=base_url('admin/logout')?>" class="nav-link">
+                      <a href="<?= base_url('admin/logout') ?>" class="nav-link">
                           <i class="nav-icon fas fa-th"></i>
                           <p>
                               Logout
